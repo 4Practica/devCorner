0ef5efc3c12dc062cb624f6cc3495c2abd71e8ba
Jose Velasquez <velasquezdenorajose@gmail.com>
2023-11-17 16:30:01 -0400

    :sparkles: config ESLINT, Prettier and Husky

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,10 +6,42 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: [
+    'dist',
+    '.eslintrc.cjs',
+    'package.json',
+    'package-lock.json',
+  ],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', '@stylistic'],
   rules: {
+    // With autofix
+    'block-spacing': 'off',
+    '@typescript-eslint/block-spacing': 'error',
+    '@typescript-eslint/no-inferrable-types': 'error',
+    '@typescript-eslint/array-type': 'error',
+    '@typescript-eslint/ban-tslint-comment': 'error',
+    'no-template-curly-in-string': 'error',
+    'no-implicit-coercion': 'error',
+    'no-lonely-if': 'error',
+    'yoda': 'error',
+    'curly': 'error',
+
+    // Without autofix
+    'no-alert': 'error',
+    'no-bitwise': 'error',
+    'no-empty-function': 'error',
+    'no-magic-numbers': 'error',
+    'no-duplicate-imports': 'error',
+    'arrow-body-style': ['error', 'always'],
+    'eqeqeq': ['error', 'always'],
+    'camelcase': [
+      'error',
+      {
+        ignoreDestructuring: true,
+        ignoreImports: true,
+      },
+    ],
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
