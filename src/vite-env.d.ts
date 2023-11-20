b3024267a195f50d448e398c6c86a7036e57b97d
bryanariadnacg <bryan.garcia@mergeto.co>
2023-11-19 23:53:23 -0500

    :wrench: Updates vite configuration

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,6 @@
 /// <reference types="vite/client" />
+declare module '@views';
+declare module '@services';
+declare module '@commn';
+declare module '@assets';
+declare module '@styles';
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,61 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
+/* import alias from '@rollup/plugin-alias'; */
 import react from '@vitejs/plugin-react'
+import path from 'path';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({command, mode}) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  const projectRootDir = path.resolve(__dirname)
+  const globalConfig = {
+    plugins: [
+      react(),
+      /* alias({
+        entries: {
+          '@utils': path.resolve(projectRootDir, '/src/js/utils'),
+          '@services': path.resolve(projectRootDir, '/src/js/services'),
+          '@views': path.resolve(projectRootDir, '/src/js/views'),
+          '@common': path.resolve(projectRootDir, '/src/js/common'),
+          '@styles': path.resolve(projectRootDir, '/src/styles'),
+          '@assets': path.resolve(projectRootDir, '/src/assets'),
+        }
+      }) */
+    ],
+    base: "/",
+    resolve: {
+      alias: {
+        '@utils': path.resolve(projectRootDir, '/src/js/utils'),
+        '@services': path.resolve(projectRootDir, '/src/js/services'),
+        '@views': path.resolve(projectRootDir, '/src/js/views'),
+        '@common': path.resolve(projectRootDir, '/src/js/common'),
+        '@styles': path.resolve(projectRootDir, '/src/styles'),
+        '@assets': path.resolve(projectRootDir, '/src/assets'),
+      }
+    }
+  }
+  if (command === 'serve') {
+    
+    return {
+      ...globalConfig,
+      publicDir: "/public",
+      server: {
+        port: 3000,
+        open: true,
+      }
+    }
+  }
+  if (command === 'build') {
+    return {
+      ...globalConfig,
+      build: {
+        outDir: "./dist",
+        assetsDir: "./app",
+        cssCodeSplit: true,
+        sourcemap: true,
+        emptyOutDir: true,
+        reportCompressedSize: true,
+        chunkSizeWarningLimit: 520,
+      }
+    }
+  }
 })
