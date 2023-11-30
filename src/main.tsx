fafc7f1928427d555020412617dc78931e327612
takove <luiss.rosalg.13@gmail.com>
2023-11-29 23:58:01 -0400

    :bricks: React router dom configuration
    installed react router dom, and created modified main.tsx to handle the structure of react router dom, created an error-page to handle any 404 errors.

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import {
+  createBrowserRouter,
+  RouterProvider
+} from 'react-router-dom'
 import './index.css'
 import '@styles/fonts.css'
 import '@styles/variables.css'
 import '@styles/responsive.css'
+import ErrorPage from './error-page.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement:<ErrorPage/>
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router ={router}/>
   </React.StrictMode>
 )
