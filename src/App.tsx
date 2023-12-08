eb2fbd1d14cd812cb7f91532cca935ddb7fc7de0
bryanstgarcia <bryan1997st@gmail.com>
2023-12-08 01:34:32 -0500

    :art: Adds navbar into view rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,7 @@
 import { HomeView } from '@views'
-import React from 'react'
-import { Navbar } from './js/common/layout'
 
 function App() {
-  return (
-    <React.Fragment>
-      <Navbar />
-      <HomeView />
-    </React.Fragment>
-  )
+  return <HomeView />
 }
 
 export default App
--- src/js/views/Home/Home.tsx
+++ src/js/views/Home/Home.tsx
@@ -1,12 +1,16 @@
-import { AppContainer } from '../../common/layout'
+import React from 'react'
+import { AppContainer, Navbar } from '../../common/layout'
 import { Hero } from './Components'
 //import styles from './Home.module.css'
 
 export const Home = () => {
   return (
-    <AppContainer>
-      <Hero />
-    </AppContainer>
+    <React.Fragment>
+      <Navbar />
+      <AppContainer>
+        <Hero />
+      </AppContainer>
+    </React.Fragment>
   )
 }
 
