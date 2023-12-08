71939daa8974724a8038955edf43e8acce36ff5e
bryanstgarcia <bryan1997st@gmail.com>
2023-12-07 22:43:09 -0500

    :sparkles: Adds navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { HomeView } from '@views'
+import React from 'react'
+import { Navbar } from './js/common/layout'
 
 function App() {
   return (
-    <>
+    <React.Fragment>
+      <Navbar />
       <HomeView />
-    </>
+    </React.Fragment>
   )
 }
 
--- src/js/common/layout/Navbar/Navbar.tsx
+++ src/js/common/layout/Navbar/Navbar.tsx
@@ -1,7 +1,33 @@
+import { Link } from 'react-router-dom'
 import styles from './Navbar.module.css'
+import { Button } from '../../components'
 
 const Navbar = () => {
-  return <nav className={styles.color}>Navbars</nav>
+  return (
+    <nav className={styles.color}>
+      <div>
+        <picture>
+          <source srcSet="" media="" />
+          <img src="" alt="" />
+        </picture>
+      </div>
+      <ul>
+        <Link to="/">Home</Link>
+        <Link to="/blog">Blog</Link>
+        <Link to="/about">About us</Link>
+      </ul>
+      <Button 
+        type="button"
+        bgColor='bg-primary'
+        fontColor='color-light'
+        text='Contact'
+        proportion='button_regular'
+        onClick={(event) => {
+          console.log(event.target) 
+        }}
+      />
+    </nav>
+  )
 }
 
 export default Navbar
