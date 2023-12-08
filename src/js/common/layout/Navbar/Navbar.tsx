037cd756ff1a64b42b10552ee100eb45d61ec714
bryanstgarcia <bryan1997st@gmail.com>
2023-12-08 01:35:17 -0500

    :sparkles: Create navbar interaction structure

--- src/js/common/layout/Navbar/Navbar.tsx
+++ src/js/common/layout/Navbar/Navbar.tsx
@@ -1,31 +1,63 @@
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
-import styles from './Navbar.module.css'
 import { Button } from '../../components'
+import styles from './Navbar.module.css'
 
 const Navbar = () => {
+  const [hamburguerIsOpen, setHamburguerIsOpen] = useState(false)
+  console.log(styles)
   return (
-    <nav className={styles.color}>
-      <div>
-        <picture>
-          <source srcSet='' media='' />
-          <img src='' alt='' />
-        </picture>
+    <nav
+      className={`${styles.navbar_box} ${
+        hamburguerIsOpen ? styles.active : ''
+      }`}
+    >
+      <div className={`${styles.navbar_main}`}>
+        <div className={styles.navbar_logo}>
+          <picture>
+            <source srcSet='' media='' />
+            <img src='' alt='' />
+          </picture>
+          <h2>{'<Img/>'}Logo</h2>
+        </div>
+
+        <div
+          className={`${styles.navbar_hamburguer}`}
+          onClick={() => {
+            setHamburguerIsOpen((prev) => {
+              return !prev
+            })
+          }}
+        >
+          <div className={styles.navbar_hamburguer__item} />
+        </div>
       </div>
-      <ul>
-        <Link to='/'>Home</Link>
-        <Link to='/blog'>Blog</Link>
-        <Link to='/about'>About us</Link>
-      </ul>
-      <Button
-        type='button'
-        bgColor='bg-primary'
-        fontColor='color-light'
-        text='Contact'
-        proportion='button_regular'
-        onClick={(event) => {
-          console.log(event.target)
-        }}
-      />
+
+      <div className={styles.navbar_content}>
+        <ul className={`${styles.navbar_content__items}`}>
+          <li className={``}>
+            <Link to='/'>Home</Link>
+          </li>
+          <li className={``}>
+            <Link to='/blog'>Blog</Link>
+          </li>
+          <li className={``}>
+            <Link to='/about'>About us</Link>
+          </li>
+        </ul>
+        <div className={``}>
+          <Button
+            type='button'
+            bgColor='bg-primary'
+            fontColor='color-light'
+            text='Contact'
+            proportion='button_small'
+            onClick={(event) => {
+              console.log(event.target)
+            }}
+          />
+        </div>
+      </div>
     </nav>
   )
 }
