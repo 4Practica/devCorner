cedf172c16e9668cf9cd6487f6ec2043e2dc752e
bryanstgarcia <bryan1997st@gmail.com>
2023-12-22 18:33:17 -0500

    :sparkles: Creates responsive and styles enums

---
+++ src/js/common/utils/enums/styles.ts
@@ -0,0 +1,13 @@
+export enum COLOR_STYLES_CLASSES {
+  LIGHT = 'color-light',
+  DARK = 'color-dark',
+  PRIMARY = 'color-primary',
+  SECONDARY = 'color-secondary',
+}
+
+export enum BG_STYLES_CLASSES {
+  LIGHT = 'bg-light',
+  DARK = 'bg-dark',
+  PRIMARY = 'bg-primary',
+  SECONDARY = 'bg-secondary',
+}
