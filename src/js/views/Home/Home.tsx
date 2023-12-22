78e5febe1eff489244d114ee9d660176937633d2
bryanstgarcia <bryan1997st@gmail.com>
2023-12-22 18:44:16 -0500

    :white_check_mark: Adds height scroll test

--- src/js/views/Home/Home.tsx
+++ src/js/views/Home/Home.tsx
@@ -1,16 +1,15 @@
-import React from 'react'
 import { AppContainer, Navbar } from '../../common/layout'
 import { Hero } from './Components'
 //import styles from './Home.module.css'
 
 export const Home = () => {
   return (
-    <React.Fragment>
-      <Navbar />
+    <div style={{ minHeight: '500vh' }}>
       <AppContainer>
+        <Navbar />
         <Hero />
       </AppContainer>
-    </React.Fragment>
+    </div>
   )
 }
 
