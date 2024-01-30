7b69604994e3a0ac2bc17c94bbe7da3d8d4c38b3
bryanstgarcia <bryan1997st@gmail.com>
2024-01-30 01:10:31 -0500

    :lipstick: Adds AppContianer adn updates navbar positioning

--- src/js/common/layout/Navbar/Navbar.tsx
+++ src/js/common/layout/Navbar/Navbar.tsx
@@ -5,6 +5,7 @@ import styles from './Navbar.module.css'
 import { NavbarHamburguer } from './components'
 import { useButtonHandler } from './Navbar.hooks'
 import { BG_STYLES_CLASSES, COLOR_STYLES_CLASSES } from '@common/utils/enums'
+import { AppContainer } from '..'
 
 const Navbar = () => {
   const [hamburguerIsOpen, setHamburguerIsOpen] = useState(false)
@@ -14,52 +15,54 @@ const Navbar = () => {
     return setHamburguerIsOpen(false)
   }, [])
   return (
-    <nav
-      className={`${styles.navbar_box} ${
-        hamburguerIsOpen ? styles.active : ''
-      } ${location.pathname === '/' ? styles.home_nav : styles.regular_nav}
-      `}
-    >
-      <div className={`${styles.navbar_main}`}>
-        <div className={styles.navbar_logo}>
-          <picture>
-            <source srcSet='' media='' />
-            <img src='' alt='' />
-          </picture>
-          <h2>{'<Img/>'}Logo</h2>
+    <AppContainer>
+      <nav
+        className={`${styles.navbar_box} ${
+          hamburguerIsOpen ? styles.active : ''
+        } ${location.pathname === '/' ? styles.home_nav : styles.regular_nav}
+        `}
+      >
+        <div className={`${styles.navbar_main}`}>
+          <div className={styles.navbar_logo}>
+            <picture>
+              <source srcSet='' media='' />
+              <img src='' alt='' />
+            </picture>
+            <h2>{'<Img/>'}Logo</h2>
+          </div>
+          <NavbarHamburguer
+            isOpen={hamburguerIsOpen}
+            handleOpen={setHamburguerIsOpen}
+          />
         </div>
-        <NavbarHamburguer
-          isOpen={hamburguerIsOpen}
-          handleOpen={setHamburguerIsOpen}
-        />
-      </div>
 
-      <div className={styles.navbar_content}>
-        <ul className={`${styles.navbar_content__items}`}>
-          {/* <NavbarLink /> */}
-          <li className={``}>
-            <Link to='/'>Home</Link>
-          </li>
-          <li className={``}>
-            <Link to='/blog'>Blog</Link>
-          </li>
-          <li className={``}>
-            <Link to='/about'>About us</Link>
-          </li>
-        </ul>
-        <div className={``}>
-          <Button
-            type='button'
-            bgColor={BG_STYLES_CLASSES.PRIMARY}
-            fontColor={COLOR_STYLES_CLASSES.LIGHT}
-            proportion={'md'}
-            onClick={useButtonHandler(navigate)}
-          >
-            Subscribe
-          </Button>
+        <div className={styles.navbar_content}>
+          <ul className={`${styles.navbar_content__items}`}>
+            {/* <NavbarLink /> */}
+            <li className={``}>
+              <Link to='/'>Home</Link>
+            </li>
+            <li className={``}>
+              <Link to='/blog'>Blog</Link>
+            </li>
+            <li className={``}>
+              <Link to='/about'>About us</Link>
+            </li>
+          </ul>
+          <div className={``}>
+            <Button
+              type='button'
+              bgColor={BG_STYLES_CLASSES.PRIMARY}
+              fontColor={COLOR_STYLES_CLASSES.LIGHT}
+              proportion={'md'}
+              onClick={useButtonHandler(navigate)}
+            >
+              Subscribe
+            </Button>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </AppContainer>
   )
 }
 
