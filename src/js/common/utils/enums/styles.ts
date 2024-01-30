f6b99d7d4de87ce6a25fb4019362890d8f5c30b8
bryanstgarcia <bryan1997st@gmail.com>
2024-01-30 01:09:21 -0500

    :lipstick: Adds new color into COLOR enum

--- src/js/common/utils/enums/styles.ts
+++ src/js/common/utils/enums/styles.ts
@@ -3,6 +3,7 @@ export enum COLOR_STYLES_CLASSES {
   DARK = 'color-dark',
   PRIMARY = 'color-primary',
   SECONDARY = 'color-secondary',
+  DARK_GREEN = 'color-dark-green',
 }
 
 export enum BG_STYLES_CLASSES {
