5ec30428c294ebf948484f8c6681b139e08177a4
bryanstgarcia <bryan1997st@gmail.com>
2024-01-30 01:11:59 -0500

    :sparkles: Creates postcard structure and adds some test PostCards
    - The PostCard component are used as placeholder, it is need to update it

--- src/js/views/BlogLandings/BlogLandings.tsx
+++ src/js/views/BlogLandings/BlogLandings.tsx
@@ -17,31 +17,42 @@ const BlogLandings = () => {
         </div>
       </AppContainer>
       <AppContainer bgColor={BG_STYLES_CLASSES.DARK} elementType='section'>
-          <div className={`padding-x padding-y ${styles['blog-posts']}`}>
-            {["0", "0" , "0", "0", "0", "0" , "0", "0"].map((_, index) => {
-              return (
-                <PostCard key={index}>
-                  <PostCard.Image 
-                    src="https://images.pexels.com/photos/6803545/pexels-photo-6803545.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" 
-                    alt='Chilling' 
-                    position='bottom right' 
-                    />
-                  <PostCard.Tags labels={[{id: crypto.randomUUID(), label: "FRONTEND", value: "frontend"}]}/>
-                  <PostCard.Author>John Doe • 19 Jan 2022</PostCard.Author>
-                  <PostCard.Title headerType={'h3'}>
-                    A small business is only as good as its tools and it is totally true.
-                  </PostCard.Title>
-                  <PostCard.Description>
-                    We've all experienced the chaos of multiple spreadsheets, 
-                    tracking and insight tools, and scrambling for the right 
-                    data at the right time.
-                  </PostCard.Description>
-                  <PostCard.CTA to={"/home"}>{"Read post"}<ArrowRightIcon /></PostCard.CTA>
-                </PostCard>
-              )
-            }
-            )}
-          </div>
+        <div className={`padding-x padding-y ${styles['blog-posts']}`}>
+          {['0', '0', '0', '0', '0', '0', '0', '0'].map((_, index) => {
+            return (
+              <PostCard key={index}>
+                <PostCard.Image
+                  src='https://images.pexels.com/photos/6803545/pexels-photo-6803545.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+                  alt='Chilling'
+                  position='bottom right'
+                />
+                <PostCard.Tags
+                  labels={[
+                    {
+                      id: crypto.randomUUID(),
+                      label: 'FRONTEND',
+                      value: 'frontend',
+                    },
+                  ]}
+                />
+                <PostCard.Author>John Doe • 19 Jan 2022</PostCard.Author>
+                <PostCard.Title headerType={'h3'}>
+                  A small business is only as good as its tools and it is
+                  totally true.
+                </PostCard.Title>
+                <PostCard.Description>
+                  We've all experienced the chaos of multiple spreadsheets,
+                  tracking and insight tools, and scrambling for the right data
+                  at the right time.
+                </PostCard.Description>
+                <PostCard.CTA to={'/home'}>
+                  {'Read post'}
+                  <ArrowRightIcon />
+                </PostCard.CTA>
+              </PostCard>
+            )
+          })}
+        </div>
       </AppContainer>
     </React.Fragment>
   )
