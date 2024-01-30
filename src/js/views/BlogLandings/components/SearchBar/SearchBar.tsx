d18544bfb07194b05e96396230d8d1770b8e6c5f
bryanstgarcia <bryan1997st@gmail.com>
2024-01-30 01:18:15 -0500

    :sparkles: Creates SearchBar structure and styles

--- src/js/views/BlogLandings/components/SearchBar/SearchBar.tsx
+++ src/js/views/BlogLandings/components/SearchBar/SearchBar.tsx
@@ -1,13 +1,20 @@
 import styles from './SearchBar.module.css'
 import search from '@assets/search.svg'
 const SearchBar = () => {
-    return (
-        <form className={`${styles['search-bar']}`}>
-            <label><img alt="Buscar" src={search} /></label>
-            <input type="search" placeholder={"Search"}onChange={() => { /* Algo */ }}/>
-        </form>
-    )
+  return (
+    <form className={`${styles['search-bar']}`}>
+      <label>
+        <img alt='Buscar' src={search} />
+      </label>
+      <input
+        type='search'
+        placeholder={'Search'}
+        onChange={() => {
+          /* Algo */
+        }}
+      />
+    </form>
+  )
 }
-
 
 export default SearchBar
