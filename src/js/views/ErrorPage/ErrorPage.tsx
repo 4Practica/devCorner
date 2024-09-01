f5eee9598c816dffb8c98964b9590ed5d9e12ac1
bryanstgarcia <bryan1997st@gmail.com>
2024-09-01 17:13:01 -0500

    :zap: Adds image into mobile version - :lipstick: update layout with grid. - :hammer: Restructure JSX

--- src/js/views/ErrorPage/ErrorPage.tsx
+++ src/js/views/ErrorPage/ErrorPage.tsx
@@ -2,35 +2,59 @@ import { AppContainer } from '@common/layout'
 import styles from './ErrorPage.module.css'
 import dots from '@assets/dots.svg'
 import wave from '@assets/wave.svg'
+import { Link, useLocation } from 'react-router-dom'
+import { Chip } from '@common/components'
+import { BG_STYLES_CLASSES } from '@common/utils/enums'
 
 const ErrorPage = () => {
+  const location = useLocation()
+  console.log(location)
   return (
-    <AppContainer>
+    <AppContainer bgColor={BG_STYLES_CLASSES.DARK}>
       <div className={`${styles.errorViewContainer}`}>
-        <div className={`${styles.leftSide}`}></div>
-
+        <div className={`${styles.dotsTopDeco}`}>
+          <img src={dots} alt='dots' width={"150"} height={"110"} />
+        </div>
+        <div className={`${styles.leftSide}`}>
+          <picture>
+            <source
+              srcSet={
+                'https://res.cloudinary.com/bryancloudinary/image/upload/v1725157271/errorCode404.webp'
+              }
+              width={320}
+              height={180}
+              media={'(min-width: 720px)'}
+            />
+            <img
+              src={
+                'https://res.cloudinary.com/bryancloudinary/image/upload/v1725157271/errorCode404.webp'
+              }
+              width={320}
+              height={180}
+            />
+          </picture>
+        </div>
         <div className={`${styles.rightSide}`}>
-          <img src={dots} alt='dots' className={`${styles.top}`} />
-
           <div className={`${styles.content}`}>
-            <span className={`${styles.pill}`}>Error 404</span>
-
-            <h2 className={`${styles.heading}`}>Oh no! Error 404.</h2>
+            <Chip chipStyle='secondary' proportion='sm' bold={true}>
+              ERROR 404
+            </Chip>
+            <h1 className={`${styles.heading}`}>Oh no! Error 404.</h1>
             <p className={`${styles.paragraph}`}>
               Something went wrong, so this page is broken.
             </p>
-
             <div className={`${styles.buttonContainer}`}>
-              <button type='button' className={`${styles.leftButton}`}>
-                Go back to Homepage
-              </button>
-              <button type='button' className={`${styles.rightButton}`}>
+              <Link to='/' className={`${styles.leftButton}`}>
+                Go back to homepage
+              </Link>
+              <Link to={location.pathname} className={`${styles.rightButton}`}>
                 Try again
-              </button>
+              </Link>
             </div>
           </div>
-
-          <img src={wave} alt='wave' className={`${styles.bottom}`} />
+        </div>
+        <div className={`${styles.dotsBottomDeco}`}>
+          <img src={wave} alt='wave' width={"150"} height={"110"} className={`${styles.bottom}`} />
         </div>
       </div>
     </AppContainer>
