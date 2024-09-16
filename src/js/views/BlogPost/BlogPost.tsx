d82d4acb4706f1c71b69c186778fe2af22011cd8
bryanstgarcia <bryan1997st@gmail.com>
2024-09-16 00:46:10 -0500

    :sparkles: Structure BlogPost

--- src/js/views/BlogPost/BlogPost.tsx
+++ src/js/views/BlogPost/BlogPost.tsx
@@ -1,18 +1,30 @@
-import { AppContainer } from '@common/layout'
-import './BlogPost.module.css'
+import styles from './BlogPost.module.css'
+/* import remarkGfm from 'remark-gfm'; // For GitHub Flavored Markdown
+import rehypePrism from 'rehype-prism'; */
+//import dummyMd from './blogPost.placeholder.md?raw'
+//import dummyPost from './ecommerce.placeholder.md?raw'
+import dummyPostTS from './typescript.placeholder.md?raw'
+import React from 'react'
+import { dummyData } from './components/Article/Article.utils'
+import { Article, RenderMD } from './components'
 
-const BlogPost = () => {
+const BlogPost: React.FC = () => {
+  const { title, shortDescription, author, date, img, tags } = dummyData
+
   return (
-    <AppContainer>
-      <h1>BlogPost landing</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore neque
-        ratione amet quisquam, saepe iste, repellendus odio odit porro, numquam
-        atque animi esse laboriosam ipsa voluptates? Voluptates quia pariatur
-        sint.
-      </p>
-    </AppContainer>
+    <main className={styles[`blog-post`]}>
+      <Article
+        title={title}
+        date={date}
+        author={author}
+        shortDescription={shortDescription}
+        tags={tags}
+        img={img}
+      >
+        <RenderMD MD={dummyPostTS} />
+      </Article>
+    </main>
   )
 }
 
-export default BlogPost;
+export default BlogPost
