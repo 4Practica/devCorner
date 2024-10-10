aa4537ca6f185a0aa7978c7c054544f78e6b7d3e
Jose Velasquez <velasquezdenorajose@gmail.com>
2024-10-09 22:00:23 -0400

    🚨 Fix linter warning

--- src/js/services/cms/strapi/getBlogPost.tsx
+++ src/js/services/cms/strapi/getBlogPost.tsx
@@ -1,11 +1,13 @@
-const STRAPI_URL = import.meta.env.VITE_CMS_URL
+// const STRAPI_URL = import.meta.env.VITE_CMS_URL
 export const getBLogPostByPath = () => {
   /**
    *  function
    * getsBlogPost using fetchData service keeping the contract
    */
   try {
-    const request = fetch(STRAPI_URL + '/')
-  } catch (error) {}
+    // const request = fetch(STRAPI_URL + '/')
+  } catch (error) {
+    // Non empty catch
+  }
   return
 }
