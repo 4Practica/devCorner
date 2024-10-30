c9e0fb04680b089964117ab3301c2dcd9240763d
bryanstgarcia <bryan1997st@gmail.com>
2024-10-30 11:57:46 -0500

    :zap: Adds categories structure

--- src/js/views/Home/Components/Categories/Categories.tsx
+++ src/js/views/Home/Components/Categories/Categories.tsx
@@ -1,12 +1,22 @@
 import styles from './Categories.module.css'
 import { Chip } from '@common/components/Chip'
 
-
 const Categories = () => {
   return (
     <div className={styles.categories}>
-      <Chip variant='primary' proportion='sm'>CATEGORIES</Chip>
-      <h2>Categories</h2>
+      <Chip variant='primary' proportion='sm'>
+        CATEGORIES
+      </Chip>
+      <h2>Explore the World of Development</h2>
+      <p>
+        Whether you're diving into new frameworks, refining your coding skills,
+        or exploring trends in tech, this space is designed for developers at
+        every level. Discover topics, gain insights, and keep your projects
+        sharp and future-ready!
+      </p>
+      <div>
+        
+      </div>
     </div>
   )
 }
