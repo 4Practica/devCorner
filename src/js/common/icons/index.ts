6d921e37f85af992e31a6476785e3adfde6c25bf
Bryan Garcia <bryan1997st@gmail.com>
2024-11-09 12:03:58 -0500

    :spakles: Adds  categories svg icons

--- src/js/common/icons/index.ts
+++ src/js/common/icons/index.ts
@@ -1,1 +1,4 @@
 export { default as ArrowRightIcon } from './ArrowRight.tsx'
+export { default as ImageIcon } from './Image.tsx'
+export { default as GearIcon } from './Gear.tsx'
+export { default as ChieldIcon } from './Chield.tsx'
