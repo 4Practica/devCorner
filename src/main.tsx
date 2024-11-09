179da74244a36041220b85f9c094b3629861e438
Bryan Garcia <bryan1997st@gmail.com>
2024-11-09 11:48:21 -0500

    :sparkles: Creates global spacing and app padding

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import '@styles/fonts.css'
 import '@styles/variables.css'
 import '@styles/responsive.css'
 import '@styles/global.css'
+import '@styles/spacing.css'
 import './index.css'
 import App from './App.tsx'
 
