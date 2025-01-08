629ef22ae1407f891d54e697f391092ef3afbfba
Jose Velasquez <velasquezdenorajose@gmail.com>
2025-01-08 17:57:37 -0400

    Revert "✨ Newsletter functionality in Footer section"
    This reverts commit c4a8ca1e4cb6e37a6635f46be01113dc9bd0a513.

--- src/js/common/layout/Footer/components/FooterForm/FooterForm.tsx
+++ src/js/common/layout/Footer/components/FooterForm/FooterForm.tsx
@@ -1,9 +1,10 @@
 import { Button } from '@common/components'
 import styles from './Form.module.css'
-import { useNewsletter } from '../../Footer.hooks'
 
 const FooterForm = () => {
-  const { handleChange, handleSubmit, data } = useNewsletter()
+  const handleSubmit = () => {
+    // I'll do something here
+  }
 
   return (
     <form onSubmit={handleSubmit} className={`${styles.top_section_right}`}>
@@ -11,10 +12,8 @@ const FooterForm = () => {
         type='text'
         name='email'
         placeholder='Email'
-        value={data.email}
         className={`${styles.input}`}
         aria-labelledby='Email'
-        onChange={handleChange}
       />
       <Button
         type='submit'
